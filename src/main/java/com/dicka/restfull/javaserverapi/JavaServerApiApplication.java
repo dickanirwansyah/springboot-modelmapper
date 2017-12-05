1a2afc15493b0ce476eb38c310ca5dcce9209086
dickanirwansyah <dickanirwansyah@gmail.com>
2017-12-05 21:44:57 +0700

    main controller

---
+++ src/main/java/com/dicka/restfull/javaserverapi/JavaServerApiApplication.java
@@ -0,0 +1,12 @@
+package com.dicka.restfull.javaserverapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaServerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaServerApiApplication.class, args);
+	}
+}
