9e446546c6370579614fd9da3f1dc22b84567217
dickanirwansyah <dickanirwansyah@gmail.com>
2017-12-05 21:52:40 +0700

    utilities dto configurer

---
+++ src/main/java/com/dicka/restfull/javaserverapi/utilities/DTO.java
@@ -0,0 +1,13 @@
+package com.dicka.restfull.javaserverapi.utilities;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DTO {
+
+    Class value();
+}
